44cf7bc7fbc8eb0f349e4879433041987f72e79c
xywang68 <xywang68@gmail.com>
2020-03-19 10:47:48 -0700

    localize settings

--- bin/findTargetImage.js
+++ bin/findTargetImage.js
@@ -12,6 +12,7 @@ const xysikulixapi = require('../lib/xysikulixapi');
 const Region = xysikulixapi.Region;
 const Screen = xysikulixapi.Screen;
 const Pattern = xysikulixapi.Pattern;
+const Settings = xysikulixapi.Settings;
 
 const argv = require('minimist')(process.argv.slice(2));
 const imagePath = (argv.imagePath != null && argv.imagePath != 'undefined') ? argv.imagePath : 'Screen';
@@ -27,11 +28,11 @@ const findImage = (imagePath, imageSimilarity, maxSim, textHint, imageWaitTime, 
   const myImageSimilarity = parseFloat(imageSimilarity);
   const myMaxSim = parseFloat(maxSim);
   const myTextHint = textHint;
-  const myImageWaitTime = parseFloat(imageWaitTime);
+  const myImageWaitTime = parseInt(imageWaitTime);
   const myImageMaxCount = imageMaxCount || 1;
 
   const findRegion = new Screen();
-  findRegion.setAutoWaitTimeout(myImageWaitTime);
+  // findRegion.setAutoWaitTimeout(java.newFloat(myImageWaitTime));
 
   try {
     var oneTarget;
@@ -75,16 +76,22 @@ const findImage = (imagePath, imageSimilarity, maxSim, textHint, imageWaitTime, 
       if (imageAction && imageAction != 'none' && imageAction != 'null') {
         for (i=0; i<returnArray.length; i++) {
           var clickRegion = new Region(returnArray[i].location.x, returnArray[i].location.y, returnArray[i].dimension.width, returnArray[i].dimension.height);
+          var mySettings = new Settings();
           switch (imageAction) {
             case 'single':
+              clickRegion.hoverSync();
+              mySettings.ClickDelay = 0.05;
               clickRegion.clickSync();
               returnArray[i].clicked = returnArray[i].center;
             break;
             case 'double':
+              clickRegion.hoverSync();
               clickRegion.doubleClickSync();
               returnArray[i].clicked = returnArray[i].center;
             break;
             case 'right':
+              clickRegion.hoverSync();
+              mySettings.ClickDelay = 0.05;
               clickRegion.rightClickSync();
               returnArray[i].clicked = returnArray[i].center;
             break;
